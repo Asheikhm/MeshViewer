264897786ef4bb774a34a49af63269dca67d0dc0
afsaneh <afsaneh1994@gmail.com>
2021-09-08 20:45:19 -0400

    first commit

---
+++ build/CMakeFiles/MeshViewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeshViewer.dir/src/main.cpp.o"
+  "MeshViewer"
+  "MeshViewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MeshViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define GLFW_INCLUDE_NONE
+#include <glad/gl.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+    std::cout<< "BYE WORLD";
+    return 0;
+}
