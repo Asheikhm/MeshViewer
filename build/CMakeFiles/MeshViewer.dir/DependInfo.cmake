900838c043c3067d35d564155038c04e44c8cc50
afsaneh <afsaneh1994@gmail.com>
2021-09-09 18:23:27 -0400

    Creating window

--- build/CMakeFiles/MeshViewer.dir/DependInfo.cmake
+++ build/CMakeFiles/MeshViewer.dir/DependInfo.cmake
@@ -1,8 +1,20 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/afsanehsheikhmiri/Documents/Afsaneh/MeshViewer/ThirdParty/glad/glad.c" "/Users/afsanehsheikhmiri/Documents/Afsaneh/MeshViewer/build/CMakeFiles/MeshViewer.dir/ThirdParty/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../ThirdParty"
+  "/usr/local/include"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/afsanehsheikhmiri/Documents/Afsaneh/MeshViewer/src/main.cpp" "/Users/afsanehsheikhmiri/Documents/Afsaneh/MeshViewer/build/CMakeFiles/MeshViewer.dir/src/main.cpp.o"
   )
@@ -11,6 +23,8 @@ set(CMAKE_CXX_COMPILER_ID "AppleClang")
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
+  "../ThirdParty"
+  "/usr/local/include"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/MeshViewer.dir/cmake_clean.cmake
+++ build/CMakeFiles/MeshViewer.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/MeshViewer.dir/ThirdParty/glad/glad.c.o"
   "CMakeFiles/MeshViewer.dir/src/main.cpp.o"
   "MeshViewer"
   "MeshViewer.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/MeshViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
